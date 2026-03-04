78d0e25088cb0a4cd870852f4b924ff5b6a93bf1
duyola2k3 <duyola2k3@gmail.com>
2026-03-04 11:42:48 +0700

    Create list.js

---
+++ list.js
@@ -0,0 +1,18 @@
+{
+  "40-2": {
+    "keyword": "bcx88",
+    "targetUrl": "bcx88.work"
+  },
+  "51-2": {
+    "keyword": "SP8BET",
+    "targetUrl": "zvk.uk.com"
+  },
+  "60-1": {
+    "keyword": "ABCXYZ",
+    "targetUrl": "abc.com"
+   },
+  "21-2": {
+    "keyword": "nohu",
+    "targetUrl": "dom.ae.org"
+  }
+}
